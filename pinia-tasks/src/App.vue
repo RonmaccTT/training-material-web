cb8c9c152af9c522d615d7bf5bb9a12c4885273d
Ronmaccms <154700667+ronmaccms@users.noreply.github.com>
2024-03-19 09:46:42 -0400

    workinh on new pinia test

---
+++ pinia-tasks/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <!-- heading -->
+    <header>
+      <img src="https://pinia.vuejs.org/logo.svg" alt="pinia logo">
+    </header>
+  </main>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+
